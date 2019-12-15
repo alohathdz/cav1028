c8b698e0f3a1defe8ff8c3a079d86d37a6094c92
alohathdz <aloha@stephack.com>
2019-12-15 22:37:31 +0700

    ีupdate

--- 1.php
+++ 1.php
@@ -4,20 +4,21 @@ if (!$_SESSION["UserLevel"]) {
   header("location: login.php");
 } else {
   ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Bootstrap 4 Dropdowns within a Navbar</title>
+  <!DOCTYPE html>
+  <html lang="en">
 
-<link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Cavalry 28th</title>
 
-</head>
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 
-<body>
+  </head>
 
-<!-- Navbar -->
+  <body>
+
+    <!-- Navbar >
 <nav class="navbar navbar-expand-lg fix-top navbar-dark bg-dark">
 <img src="photos/logo.png" width="30" height="30" alt="">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -32,32 +33,92 @@ if (!$_SESSION["UserLevel"]) {
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <img src="photos/1670200172231.jpg" class="rounded-circle" width="30" height="30" alt="">
+         <img src="</?php echo $_SESSION["Photo"]; ?>" class="rounded-circle" width="30" height="30" alt="">
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">ข้อมูลส่วนตัว</a>
+          <a class="dropdown-item" href="#">เปลี่ยนรหัสผ่าน</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">ออกจากระบบ</a>
         </div>
       </li>
     </ul>
   </div>
-</nav>
+</nav -->
 
-<!-- Jumbotron -->
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">ยินดีต้อนรับ</h1>
-    <p class="lead">คุณ วรดร บุญเขต</p>
-  </div>
-</div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="photos/logo.png" width="30" height="30" alt="">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">หน้าหลัก <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ทำเนียบกองพัน</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img src="<?php echo $_SESSION["Photo"]; ?>" class="rounded-circle" width="30" height="30" alt="">
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">ข้อมูลส่วนตัว</a>
+              <a class="dropdown-item" href="#">เปลี่ยนรหัสผ่าน</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">ออกจากระบบ</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
-<script src="node_modules\jquery\dist\jquery.min.js"></script>
-<script src="node_modules\popper.js\dist\popper.min.js"></script>
-<script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">ยินดีต้อนรับ</h1>
+        <p class="lead"><?php echo $_SESSION["User"]; ?></p>
+      </div>
+    </div>
+
+    <!-- Card -->
+    <div class="card text-center">
+      <div class="card-header">
+        ข้อมูลส่วนบุคคล
+      </div>
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $_SESSION["User"]; ?></h5>
+        <img src="photos/null.jpg" class="img-thumbnail" width="200" height="200" alt="">
+        <p class="card-text">เลขประจำตัวประชาชน</p>
+        <p class="card-text">เลขประจำตัวข้าราชการ</p>
+        <p class="card-text">ตำแหน่ง</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-footer text-muted">
+        2 days ago
+      </div>
+    </div>
+
+    <script src="node_modules\jquery\dist\jquery.min.js"></script>
+    <script src="node_modules\popper.js\dist\popper.min.js"></script>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
 
 
-</body>
-</html>
+  </body>
+
+  </html>
 <?php } ?>
